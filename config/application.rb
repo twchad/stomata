98ef5a27aab01f4d1212552d44a3435f84397641
Badrinath Janakiraman <badri.j@gmail.com>
2012-11-09 13:35:51 -0800

    Fix types

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Stomata
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.paths << Rails.root.join("app", "assets", "fonts", "open-sans-cond")
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
